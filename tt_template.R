0f2652625793b98e9164bcd1d7e0a079b818fc3d
ferreirav <87486050+ferreirav@users.noreply.github.com>
2021-10-15 02:48:05 +0100

    TT Template created

---
+++ tt_template.R
@@ -0,0 +1,11 @@
+# TidyTuesday Training Series
+# R Script Template
+# Author: Vitor Ferreira
+# October 2021
+
+#Load packages
+library(tidyverse)
+library(tidytuesdayR)
+
+# Load data
+tt_data <- tt_load("")
