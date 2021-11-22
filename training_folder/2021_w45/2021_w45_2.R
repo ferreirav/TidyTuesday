049d4a1a78fbb8acc9100bc48032a306bf47f8e0
ferreirav <vitorlcferreira@gmail.com>
2021-11-22 08:18:46 +0000

    Elaboration of African densities map

--- training_folder/2021_w45/2021_w45_2.R
+++ training_folder/2021_w45/2021_w45_2.R
@@ -2,14 +2,61 @@
 # R Script for Week 45
 # Data on African Conflits 
 
-# Code Sourced from: https://github.com/marthaluka/TidyTuesday/blob/main/30DayMapChallenge/conflict_in_Africa.R
-# Author: Martha Luka
+# Code Sourced from: https://github.com/gkaramanis/tidytuesday/blob/master/2021/2021-week45/afrimapr.R
+# Author: Georgios Karamanis
 # November 2021
 
-#Load libraries-----------------------------------------------------------------
 
-install.packages("spDataLarge", repos = "https://nowosad.github.io/drat/", type = "source")
-# install.packages("pacman") # In case need to be installed 
-require("pacman")
-pacman::p_load(spData, spDataLarge, sf, tidyverse, janitor, ggrepel, viridis, hrbrthemes,
-               RColorBrewer, ggtext, cowplot, patchwork)
+# Get african data
+remotes::install_github("afrimapr/afrilearndata")
+
+
+#Load libraries-----------------------------------------------------------------
+library(afrilearndata)
+library(tidyverse)
+library(paletteer)
+library(pals)
+library(here)
+
+getwd()
+
+# Data
+
+afripop_df <- afripop2020 %>% 
+  as.data.frame(xy = T) %>% 
+  rename(pop = 3) %>% 
+  filter(!is.na(pop)) %>% 
+  mutate(
+    pop2 = case_when(
+      pop <= 500 ~ pop,
+      TRUE ~ 600
+    )
+  )
+
+str(afripop_df)
+
+f1 = "Porpora"
+f2 = "Public Headline"
+
+africa_plot <- ggplot(afripop_df) +
+  geom_tile(aes(x, y, fill = pop2)) +
+  annotate("text", -5, -5, label = "Africa", family = "Times New Roman",
+           size = 22, fontface = "bold", color = "grey97") +
+  scale_fill_gradientn(colours = viridis(100), breaks = seq(0, 600, 100), labels = c(seq(0, 500, 100), ">500")) +
+  guides(fill = guide_colorbar(title = "Population density\n(people/km2)", label.position = "left", title.hjust = 0.5)) +
+  labs(caption = "Data: afrilearndata - Inspired by Georgios Karamanis - Made by Vitor Ferreira") +
+  coord_fixed() +
+  theme_void(base_family = "Times New Roman", base_size = 14) +
+  theme(
+    legend.position = c(0.22, 0.27),
+    legend.key.width = unit(0.5, "line"),
+    legend.title = element_text(size = 16, margin = margin(0, 0, 10, -20), color = "grey85", lineheight = 1.1),
+    legend.text = element_text(size = 11, color = "grey85"),
+    plot.background = element_rect(fill = "#58507E", colour = NA),
+    plot.caption = element_text(hjust = 0.5, colour = "grey80"),
+    plot.margin = margin(10, 10, 10, 10)
+  )
+
+africa_plot
+
+ggsave("./training_folder/2021_w45/africa_densities.png", device = "png")
